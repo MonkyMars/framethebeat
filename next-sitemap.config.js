063e82f4c590bd67a043278f87da578cde535c1f
LeviNoppers <levinoppers69@gmail.com>
2025-01-04 22:38:02 +0100

    Enhance sitemap configuration and update robots.txt for improved SEO and indexing

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,4 +1,16 @@
-module.exports = {
+/** @type {import('next-sitemap').IConfig} */
+const config = {
   siteUrl: 'https://www.framethebeat.com',
   generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/admin/*', '/private/*'],
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.framethebeat.com/server-sitemap.xml',
+    ],
+  },
 }
+
+export default config
