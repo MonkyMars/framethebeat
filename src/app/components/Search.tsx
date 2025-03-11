2e66eac050f0820e67dc18c6495628449f913046
Levi Noppers <levinoppers69@gmail.com>
2025-03-11 17:42:01 +0100

    Add aria-label to search button for improved accessibility

--- src/app/components/Search.tsx
+++ src/app/components/Search.tsx
@@ -19,7 +19,7 @@ const SearchComponent = () => {
       </h2>
       <div className="flex justify-center items-center gap-4 p-4 bg-[rgba(var(--background-rgb),0.05)] backdrop-blur-md border-b border-[rgba(var(--theme-rgb),0.1)] transition-transform duration-300 w-full">
         <input type="text" placeholder="Search by album or artist" value={searchValue} onChange={(e) => setSearchValue(e.target.value)} className="p-3 bg-transparent border border-[rgba(var(--theme-rgb),0.3)] rounded-md text-foreground text-base transition-all duration-300 focus:outline-none focus:border-[rgba(var(--theme-rgb),0.6)] focus:shadow-[0_0_8px_rgba(var(--theme-rgb),0.2)] w-full" />
-        <button onClick={handleSearch} className="p-3 bg-gradient-to-br from-[var(--theme)] to-[rgba(var(--theme-rgb),0.8)] text-foreground text-base font-semibold rounded-md transition-all duration-300 hover:translate-y-[-2px] hover:shadow-[0_6px_20px_rgba(var(--theme-rgb),0.3)]">
+        <button aria-label='Continue' onClick={handleSearch} className="p-3 bg-gradient-to-br from-[var(--theme)] to-[rgba(var(--theme-rgb),0.8)] text-foreground text-base font-semibold rounded-md transition-all duration-300 hover:translate-y-[-2px] hover:shadow-[0_6px_20px_rgba(var(--theme-rgb),0.3)]">
           <ArrowRight size={24} />
         </button>
       </div>
